30502c1ddae56289b6bfb3f2298067a7823e8d16
vgspedro <vgspedro@gmail.com>
2018-08-26 15:11:25 +0100

    add project

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,119 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+/*https://github.com/nojacko/email-validator*/
+use EmailValidator\EmailValidator;
+
+class HomeController extends AbstractController
+{
+
+    public function index(Request $request)
+    {
+
+        $services[] = array('img'=>'images/manip.png','name'=>'PHOTO<br>MANIPULATION','alt'=>'manipular imagens','class'=>'PHOTO_MANIPULATION');
+        $services[] = array('img'=>'images/editorial.png','name'=>'EDITORIAL<br>DESIGN', 'alt'=>'design editorial','class'=>'EDITORIAL_DESIGN');
+        $services[] = array('img'=>'images/3d.png', 'name'=>'3D<br>RENDERING', 'alt'=>'renderizar 3d','class'=>'3D_RENDERING');
+        $services[] = array('img'=>'images/logo.png', 'name'=>'LOGO<br>DESIGN', 'alt'=>'design logo','class'=>'LOGO_DESIGN');
+        $services[] = array('img'=>'images/video.png', 'name'=>'VIDEO<br>EDITING', 'alt'=>'edição video','class'=>'VIDEO_EDITING');
+        $services[] = array('img'=>'images/web.png', 'name' =>'WEB<br>DESIGN', 'alt'=>'web design','class'=>'WEB_DESIGN');
+        $services[] = array('img'=>'images/foto.png', 'name' =>'PHOTOGRAPHY', 'alt'=>'fotografia','class'=>'PHOTOGRAPHY');
+        $services[] = array('img'=>'images/illustration.png', 'name'=>'ILLUSTRATION', 'alt'=>'ilustração','class'=>'ILLUSTRATION');
+        $services[] = array('img'=>'images/stat.png', 'name'=>'STATIONARY', 'alt'=>'stationary', 'class'=>'STATIONARY');
+
+        $skills[] = array('border'=>'#86befb', 'img'=>'images/ps.jpg', 'name'=>'ADOBE<br>PHOTOSHOP', 'alt'=>'adobe photoshop');
+        $skills[] = array('border'=>'#FF7B00', 'img'=>'images/ai.jpg', 'name'=>'ADOBE<br>ILLUSTRATOR', 'alt'=>'adobe illustrator');
+ 		$skills[] = array('border'=>'#ff58a0', 'img'=>'images/id.jpg', 'name'=>'ADOBE<br>INDESIGN', 'alt'=>'photo manipulation');
+ 		$skills[] = array('border'=>'#d473f8', 'img'=>'images/pr.jpg', 'name'=>'ADOBE<br>PREMIERE', 'alt'=>'photo manipulation');
+ 		$skills[] = array('border'=>'#101152', 'img'=>'images/c4d.png', 'name'=>'CINEMA<br>4D', 'alt'=>'photo manipulation');
+ 		$skills[] = array('border'=>'#84cd62', 'img'=>'images/paint.png', 'name'=>'MICROSOFT<br>PAINT', 'alt'=>'photo manipulation');
+ 		$skills[] = array('border'=>'#2f0300', 'img'=>'images/joomla.png', 'name'=>'JOOMLA<br>&nbsp;', 'alt'=>'photo manipulation');
+        $skills[] = array('border'=>'#40871d', 'img'=>'images/android.png', 'name'=>'ANDROID<br>&nbsp;', 'alt'=>'photo manipulation');
+        $skills[] = array('border'=>'#E9E9E9', 'img'=>'images/jquery.png', 'name'=>'JQUERY<br>&nbsp;', 'alt'=>'photo manipulation');
+        $skills[] = array('border'=>'#6E5400', 'img'=>'images/js.png', 'name'=>'JAVASCRIPT<br>&nbsp;', 'alt'=>'photo manipulation');
+        $skills[] = array('border'=>'#791900', 'img'=>'images/html5.png', 'name'=>'HTML5<br>&nbsp;', 'alt'=>'photo manipulation');
+        $skills[] = array('border'=>'#014574', 'img'=>'images/html3.png', 'name'=>'CSS3<br>&nbsp;', 'alt'=>'photo manipulation');
+		$skills[] = array('border'=>'#FFE4B7', 'img'=>'images/mysql.png', 'name'=>'MYSQL<br>&nbsp;', 'alt'=>'photo manipulation');
+		$skills[] = array('border'=>'#000000', 'img'=>'images/sf.png', 'name'=>'SYMFONY<br>&nbsp;', 'alt'=>'photo manipulation');
+		$skills[] = array('border'=>'#EAFFB4', 'img'=>'images/twig.jpg', 'name'=>'TWIG<br>&nbsp;', 'alt'=>'photo manipulation');
+		$skills[] = array('border'=>'#222430', 'img'=>'images/php.jpg', 'name'=>'PHP<br>&nbsp;', 'alt'=>'photo manipulation');
+
+        $menu[] =  array('name'=>'LATEST<br>WORKS', 'tag'=>'latest-works', 'class'=>'LATEST_WORKS');
+        $menu[] =  array('name'=>'SERVICES', 'tag'=>'services', 'class'=>'SERVICES');
+        $menu[] =  array('name'=>'SKILLS', 'tag'=>'skills', 'class'=>'SKILLS');
+        $menu[] =  array('name'=>'CONTACT<br>US', 'tag'=>'contacts','class'=>'CONTACT_US');
+
+        return $this->render('base.html.twig', array(
+            'services' => $services,
+            'skills' => $skills,
+            'menus' => $menu
+        ));
+    }
+
+    public function email(Request $request, \Swift_Mailer $mailer){
+
+        $err = array();
+
+        $email ='';
+
+        $request->request->get('name') ? $name = ucwords($request->request->get('name')) : $err[] = 'NAME';
+        $request->request->get('email') ? $email = strtolower($request->request->get('email')) : $err[] = 'EMAIL';
+        $request->request->get('msn') ? $msn = $request->request->get('msn') : $err[] = 'MSN';
+        $request->request->get('locale');
+
+        /*CHECK IF EMAIL IF VALID*/
+        if($email){
+            $validator = new \EmailValidator\Validator();
+            $validator->isEmail($email) ? false : $err[] = 'EMAIL_INVALID';
+            $validator->isSendable($email) ? false : $err[] = 'EMAIL_INVALID';
+            $validator->hasMx($email) ? false : $err[] = 'EMAIL_INVALID';
+            $validator->hasMx($email) != null ? false : $err[] = 'EMAIL_INVALID';
+            $validator->isValid($email) ? false : $err[] = 'EMAIL_INVALID';
+        }
+
+        if($err)
+            return new JsonResponse(array('message' => $err, 'status' => 0));
+
+        $transport = (new \Swift_SmtpTransport('mail.darioviegas.com', 465, 'ssl'))
+            ->setUsername('info@darioviegas.com')
+            ->setPassword('info#2018');
+
+            $mailer = new \Swift_Mailer($transport);
+
+            $message = (new \Swift_Message('Contato'))
+            
+                ->setFrom(['info@darioviegas.com' => 'darioviegas'])
+                ->setTo([$email => $name])
+                ->setBcc(['info@darioviegas.com' => 'darioviegas'])
+                ->setBody(
+                $this->renderView('emails/contact.html.twig', array(
+                    'name' => $name,
+                    'msn' => $msn
+                    )                           
+                        ),
+                    'text/html'
+                )
+                ->addPart('Olá '.$name.', em breve entraremos em contato. Mensagem: '.$msn.' Obrigado, DV.', 'text/plain');
+            
+            $message->getHeaders()->addTextHeader('List-Unsubscribe', 'http://darioviegas.com'); 
+            
+            $mailer->send($message);
+                
+        return new JsonResponse(array('message' => 'success', 'status' => 1));
+           
+    }
+
+
+}
+
+?>
+
+
---
+++ src/Controller/TranslationController.php
@@ -0,0 +1,114 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class TranslationController extends AbstractController
+{
+
+    public function translate(Request $request) {
+    
+        $lang = $request->request->get('lang');
+    
+        $lang == 'en' ? 
+            
+            $response = array('lang' => $lang, 'data' => $this->translationEn()) : 
+            $response = array('lang' => $lang, 'data' => $this->translationPt());
+    
+        return new JsonResponse($response);
+    }
+
+    private function translationPt(){
+        return array(
+            'error' => 'Erro',
+            'success' => 'Sucesso',
+            'close' =>'Fechar',
+            'sorry' => 'Lamentamos',
+            'required' => 'Obrigatório *',
+            'wifi_error' => 'Por favor verifique a ligação Wi-fi/Internet, e tente novamente!',
+            'thank_you' => 'Em breve entraremos em contato. Obrigado',
+            'check' => 'Verifique',
+            'email_invalid' =>'EMAIL INVÁLIDO', 
+            'submit' => 'ENVIAR',
+            'name' => 'NOME',
+            'email' => 'EMAIL',
+            'msn' => 'MENSAGEM',
+            'latest_works_inline' => 'OBRA',
+            'contact' => 'CONTATO',
+            'contact_us_inline' => 'CONTATO',
+
+            'menu' => array(
+                'latest_works' => 'OBRA',
+                'services' => 'SERVIÇO',
+                'skills' => 'TALENTO',
+                'contact_us' => 'CONTATO'),
+
+            'services' => array(
+                'photo_manipulation' => 'MANIPULAÇÃO<br>IMAGEM',
+                'editorial_design' => 'DESIGN<br>EDITORIAL',
+                'd_rendering' => 'RENDERIZAÇÃO<br>3D',
+                'logo_design' => 'LOGO<br>DESIGN',
+                'video_editing' => 'EDIÇÃO<br>VIDEO',
+                'web_design' => 'WEB<br>DESIGN',
+                'photography' => 'FOTOGRAFIA',
+                'illustration' => 'ILUSTRAÇÃO',
+                'stationary' => 'STATIONARY'),
+
+            'cookies' => array(
+                'txt' => 'Este site utiliza cookies. Ao navegar no site estará a consentir a sua utilização.',
+                'link' => 'Saiba mais sobre o uso de cookies.',
+                'btn' => 'Aceitar')
+        );
+    }
+
+
+    private function translationEn(){
+        return array(
+            'error' => 'Error',
+            'success' => 'Success',
+            'close' =>'Close',
+            'sorry'=>'Sorry',
+            'required'=>'Required *',
+            'wifi_error' =>'Please check Wi-fi/Internet connection, and try again!',
+            'thank_you' => 'Soon we will contact. Thank you',
+            'check' => 'Check',
+            'email_invalid' =>'INVALID EMAIL',
+            'submit' => 'SEND',
+            'name' => 'NAME',
+            'email' => 'EMAIL',
+            'msn' => 'MESSAGE',
+            'latest_works_inline' => 'LATEST WORKS',
+            'contact_us_inline' => 'CONTACT US',
+            'contact' => 'CONTACT',
+
+            'menu' => array(
+                'latest_works'=>'LATEST<br>WORKS',
+                'services'=>'SERVICES',
+                'skills'=>'SKILLS',
+                'contact_us'=>'CONTACT<br>US'),
+
+            'services' => array(
+                'photo_manipulation' => 'PHOTO<br>MANIPULATION',
+                'editorial_design' => 'EDITORIAL<br>DESIGN',
+                'd_rendering' => '3D<br>RENDERING',
+                'logo_design' => 'LOGO<br>DESIGN',
+                'video_editing' => 'VIDEO<br>EDITING',
+                'web_design' => 'WEB<br>DESIGN',
+                'photography' => 'PHOTOGRAPHY',
+                'illustration' => 'ILLUSTRATION',
+                'stationary' => 'STATIONARY'),
+
+            'cookies' => array(
+                'txt'=>'This website uses cookies to ensure you get the best experience on our website.',
+                'link'=>'Know more about cookies.',
+                'btn' =>'Got It!')
+        );
+    }
+}
+
+?>
